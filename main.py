88c56e9a3f769a779ef0d070ea7d88bd1b926620
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-04 10:17:32 -0600

    Adds app.get, post, put, delete in player & course

--- main.py
+++ main.py
@@ -17,6 +17,36 @@ async def list_courses():
     pass
 
 
+@app.post("/courses")
+async def add_course():
+    pass
+
+
+@app.put("/course/{course_id}")
+async def update_course(course_id):
+    pass
+
+
+@app.delete("course/{course_id}")
+async def delete_course(course_id):
+    pass
+
+
 @app.get("/players")
 async def list_players():
+    pass
+
+
+@app.post("/players")
+async def add_player():
+    pass
+
+
+@app.put("/player/{player_id}")
+async def update_player(player_id):
+    pass
+
+
+@app.delete("/player/{player_id}")
+async def delete_player(player_id):
     pass
